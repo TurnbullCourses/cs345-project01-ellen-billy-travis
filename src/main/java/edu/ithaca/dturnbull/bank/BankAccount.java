fad192f59aba3d20381c25a161f7d46eb8fdae2a
ellen6820 <echapman@ithaca.edu>
2022-02-11 11:11:40 -0500

    Deposit and isAmountValid Tests

--- src/main/java/edu/ithaca/dturnbull/bank/BankAccount.java
+++ src/main/java/edu/ithaca/dturnbull/bank/BankAccount.java
@@ -27,6 +27,25 @@ public class BankAccount {
     }
 
     /**
+     * 
+     * @param amount
+     * @return true if the amount is positive and has two decmial points or less
+     * @throws IllegalArgumentException if too many decimals are entered
+     */
+    public static boolean isAmountValid(double amount){
+        return false;
+    }
+
+    /**
+     * 
+     * @param amount
+     * increases the balance by the amount is the amount is non-negative
+     */
+    public void deposit(double amount){
+
+    }
+
+    /**
      * @post reduces the balance by amount if amount is non-negative and smaller than balance
      */
     public void withdraw (double amount) throws InsufficientFundsException{
--- src/test/java/edu/ithaca/dturnbull/bank/BankAccountTest.java
+++ src/test/java/edu/ithaca/dturnbull/bank/BankAccountTest.java
@@ -14,6 +14,33 @@ class BankAccountTest {
     }
 
     @Test
+    void isAmountValidTest(){
+        //Valid Cases
+        assertTrue(BankAccount.isAmountValid(10.00)); 
+        assertTrue(BankAccount.isAmountValid(1895.63));
+        assertTrue(BankAccount.isAmountValid(170)); // no decimals
+
+        //Invalid Cases
+        assertThrows(IllegalArgumentException.class, () -> BankAccount.isAmountValid(150.232)); // too many decimals
+        assertThrows(IllegalArgumentException.class, () -> BankAccount.isAmountValid(-95.12)); // Negative
+        assertThrows(IllegalArgumentException.class, () -> BankAccount.isAmountValid(-50000)); // Negative edge case
+
+    }
+
+    @Test
+    void depositTest() throws IllegalArgumentException{
+        BankAccount bankAccount = new BankAccount("b@j.com", 200);
+        
+        bankAccount.deposit(100);
+        assertEquals(300, bankAccount.getBalance(), 0.001); // valid deposit
+        bankAccount.deposit(189.99);
+        assertEquals(489.99, bankAccount.getBalance(), 0.001); // valid deposit
+        assertThrows(IllegalArgumentException.class, () -> bankAccount.deposit(-500)); // negative input 
+        assertThrows(IllegalArgumentException.class, () -> bankAccount.deposit(500.555)); // too many decimals
+
+    }
+
+    @Test
     void withdrawTest() throws InsufficientFundsException{
         BankAccount bankAccount = new BankAccount("a@b.com", 200);
         bankAccount.withdraw(100);
@@ -26,8 +53,6 @@ class BankAccountTest {
     void isEmailValidTest(){
         assertTrue(BankAccount.isEmailValid( "a@b.com"));   // valid email address
         assertFalse( BankAccount.isEmailValid(""));         // empty string
-     
-        
     }
 
     @Test
