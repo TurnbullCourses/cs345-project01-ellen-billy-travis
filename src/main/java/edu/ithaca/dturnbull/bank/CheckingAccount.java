f5312ca15666b7ce0ec014234ffbe840a65b24bc
Billy Winters <wwinters@ithaca.edu>
2022-02-11 11:28:32 -0500

    added abstract classes and checking and savings

---
+++ src/main/java/edu/ithaca/dturnbull/bank/CheckingAccount.java
@@ -0,0 +1,53 @@
+package edu.ithaca.dturnbull.bank;
+
+public class CheckingAccount extends abstractBankAccount{
+
+    @Override
+    double getBalance() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    String getEmail() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    boolean isAmountValid(double amount) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    void deposit(double amount) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    void withdraw(double amount) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    void transfer(double amount) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    boolean isEmailValid(String email) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    void checkHistory() {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
