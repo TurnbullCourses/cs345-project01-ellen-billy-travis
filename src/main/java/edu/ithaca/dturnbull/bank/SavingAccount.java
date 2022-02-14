5d8772f2b3d3e69f6d72197a2bbd25db65f2585e
tjones4261 <73116929+tjones4261@users.noreply.github.com>
2022-02-13 20:39:54 -0500

    Put javadoc comments in savings&checking accounts

--- src/main/java/edu/ithaca/dturnbull/bank/SavingAccount.java
+++ src/main/java/edu/ithaca/dturnbull/bank/SavingAccount.java
@@ -5,6 +5,14 @@ public class SavingAccount extends BankAccount{
     private double interestRate;
     private double maxDailyWithdrawAmount;
 
+    /**
+     * The constructor for the SavingAccount class. A new SavingsAccount will start with a user-determined
+     * amount, interest rate, and maximum daily withdraw amount. It will also start with open status,
+     * and history that only includes "Account Opened"
+     * @param startingBalance
+     * @param startingInterestRate
+     * @param maxWithdraw
+     */
     public SavingAccount(double startingBalance, double startingInterestRate, double maxWithdraw){
         if(isAmountValid(startingBalance))
             this.balance = startingBalance;
@@ -14,11 +22,15 @@ public class SavingAccount extends BankAccount{
         maxDailyWithdrawAmount = maxWithdraw;
 
         status = BankAccountStatus.OPEN;
-        accountType = BankAccountType.CHECKING;
-        history = new String[]{"Account Created"};
+        accountType = BankAccountType.SAVINGS;
+        history = new String[]{"Account Opened"};
     }
 
 
+    /**
+     * Adds interest to the balace
+     * @post interest is added to the account's balance
+     */
     public void compoundDailyInterest(){
         
     }
