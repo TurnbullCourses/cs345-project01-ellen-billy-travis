24384cb178392640b15413091c7929b96a1d830f
tjones4261 <73116929+tjones4261@users.noreply.github.com>
2022-02-14 10:45:16 -0500

    Added javadoc comments to Teller

--- src/main/java/edu/ithaca/dturnbull/bank/Teller.java
+++ src/main/java/edu/ithaca/dturnbull/bank/Teller.java
@@ -12,4 +12,44 @@ public abstract class Teller {
     public boolean userLogin(User user, String email, String password){
         return user.confirmCredentials(email, password);
     }
+
+    /**
+     * Displays the balance for the given account
+     * @param account - the account to display the balance from
+     */
+    public void checkBalance(BankAccount account){
+
+    }
+
+    /**
+     * Subtracts the given amount from the given account
+     * @param account - the account being withdrawed from
+     * @param amount -  the amount wished to be withdrawn
+     */
+    public void withdrawFromAccount(BankAccount account, double amount){
+
+    }
+
+    public void depositToAccount(BankAccount account, double amount){
+        
+    }
+
+    /**
+     * Subtracts the given from one account and adds it to another account
+     * @param fromAccount - The account that money is being taken form
+     * @param toAccount -  the account that is receiving money
+     * @param amount - the amount being transferred
+     */
+    public void transferBetweenAccount(BankAccount fromAccount, BankAccount toAccount, double amount){
+        
+    }
+
+    /**
+     * Returns the history of the account
+     * @param account - the account that the history is retrieved from
+     * @return the account history
+     */
+    public String[] getAccountHistory(BankAccount account){
+        return account.getHistory();
+    } 
 }
